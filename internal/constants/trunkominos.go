96fa2cd2d4f884ff7074569a52906ab7888ebc2f
benlsims <ben.l.sims@gmail.com>
2023-10-01 21:04:23 -0600

    Added all kinds of things, and a turtle

--- internal/constants/trunkominos.go
+++ internal/constants/trunkominos.go
@@ -5,43 +5,45 @@ import "ludum-dare-54/pkg/world"
 var (
 	//Trunkominos
 	TrunkominoTypes = map[TrunkominoType][]world.Coords{
-		SizeOnebyOne:    {{0, 0}},
-		SizeTwobyOne:    {{0, 0}, {1, 0}},
-		SizeOnebyTwo:    {{0, 0}, {0, 1}},
-		SizeThreebyOne:  {{0, 0}, {1, 0}, {2, 0}},
-		SizeOnebyThree:  {{0, 0}, {0, 1}, {0, 2}},
-		SizeFourbyOne:   {{0, 0}, {1, 0}, {2, 0}, {3, 0}},
-		SizeOnebyFour:   {{0, 0}, {0, 1}, {0, 2}, {0, 3}},
-		SizeTwobyTwo:    {{0, 0}, {1, 0}, {0, 1}, {1, 1}},
-		SizeThreebyTwo:  {{0, 0}, {1, 0}, {0, 1}, {1, 1}, {2, 0}, {2, 1}},
-		SizeFourbyTwo:   {{0, 0}, {1, 0}, {0, 1}, {1, 1}, {2, 0}, {2, 1}, {3, 0}, {3, 1}},
-		SizeTwobyThree:  {{0, 0}, {1, 0}, {0, 1}, {1, 1}, {0, 2}, {1, 2}},
-		SizeTwobyFour:   {{0, 0}, {1, 0}, {0, 1}, {1, 1}, {0, 2}, {1, 2}, {0, 3}, {1, 3}},
-		SizeTwobyFourL:  {{0, 0}, {1, 0}, {0, 1}, {0, 2}, {0, 3}},
-		SizeFourbyThree: {{0, 0}, {1, 0}, {0, 1}, {1, 1}, {2, 0}, {2, 1}, {3, 0}, {3, 1}, {0, 2}, {1, 2}, {2, 2}, {3, 2}},
-		SizeTwobyThreeP: {{0, 0}, {0, 1}, {1, 1}, {0, 2}, {1, 2}},
+		SizeOnebyOne:              {{0, 0}},
+		SizeTwobyOne:              {{0, 0}, {1, 0}},
+		SizeOnebyTwo:              {{0, 0}, {0, 1}},
+		SizeThreebyOne:            {{0, 0}, {1, 0}, {2, 0}},
+		SizeOnebyThree:            {{0, 0}, {0, 1}, {0, 2}},
+		SizeFourbyOne:             {{0, 0}, {1, 0}, {2, 0}, {3, 0}},
+		SizeOnebyFour:             {{0, 0}, {0, 1}, {0, 2}, {0, 3}},
+		SizeTwobyTwo:              {{0, 0}, {1, 0}, {0, 1}, {1, 1}},
+		SizeThreebyTwo:            {{0, 0}, {1, 0}, {0, 1}, {1, 1}, {2, 0}, {2, 1}},
+		SizeFourbyTwo:             {{0, 0}, {1, 0}, {0, 1}, {1, 1}, {2, 0}, {2, 1}, {3, 0}, {3, 1}},
+		SizeTwobyThree:            {{0, 0}, {1, 0}, {0, 1}, {1, 1}, {0, 2}, {1, 2}},
+		SizeTwobyFour:             {{0, 0}, {1, 0}, {0, 1}, {1, 1}, {0, 2}, {1, 2}, {0, 3}, {1, 3}},
+		SizeTwobyFourL:            {{0, 0}, {1, 0}, {0, 1}, {0, 2}, {0, 3}},
+		SizeFourbyThree:           {{0, 0}, {1, 0}, {0, 1}, {1, 1}, {2, 0}, {2, 1}, {3, 0}, {3, 1}, {0, 2}, {1, 2}, {2, 2}, {3, 2}},
+		SizeTwobyThreeP:           {{0, 0}, {0, 1}, {1, 1}, {0, 2}, {1, 2}},
+		SizeThreebyTwoUpsideDownT: {{0, 0}, {1, 0}, {2, 0}, {1, 1}},
 	}
 )
 
 type TrunkominoType string
 
 const (
-	UndefinedTrunkominoType = "Undefined"
-	SizeOnebyOne            = "SizeOnebyOne"
-	SizeTwobyOne            = "SizeTwobyOne"
-	SizeOnebyTwo            = "SizeOnebyTwo"
-	SizeThreebyOne          = "SizeThreebyOne"
-	SizeOnebyThree          = "SizeOnebyThree"
-	SizeFourbyOne           = "SizeFourbyOne"
-	SizeOnebyFour           = "SizeOnebyFour"
-	SizeTwobyTwo            = "SizeTwobyTwo"
-	SizeThreebyTwo          = "SizeThreebyTwo"
-	SizeFourbyTwo           = "SizeFourbyTwo"
-	SizeTwobyThree          = "SizeTwobyThree"
-	SizeTwobyFour           = "SizeTwobyFour"
-	SizeTwobyFourL          = "SizeTwobyFourL"
-	SizeFourbyThree         = "SizeFourbyThree"
-	SizeTwobyThreeP         = "SizeTwobyThreeP"
+	UndefinedTrunkominoType   = "Undefined"
+	SizeOnebyOne              = "SizeOnebyOne"
+	SizeTwobyOne              = "SizeTwobyOne"
+	SizeOnebyTwo              = "SizeOnebyTwo"
+	SizeThreebyOne            = "SizeThreebyOne"
+	SizeOnebyThree            = "SizeOnebyThree"
+	SizeFourbyOne             = "SizeFourbyOne"
+	SizeOnebyFour             = "SizeOnebyFour"
+	SizeTwobyTwo              = "SizeTwobyTwo"
+	SizeThreebyTwo            = "SizeThreebyTwo"
+	SizeFourbyTwo             = "SizeFourbyTwo"
+	SizeTwobyThree            = "SizeTwobyThree"
+	SizeTwobyFour             = "SizeTwobyFour"
+	SizeTwobyFourL            = "SizeTwobyFourL"
+	SizeFourbyThree           = "SizeFourbyThree"
+	SizeTwobyThreeP           = "SizeTwobyThreeP"
+	SizeThreebyTwoUpsideDownT = "SizeThreebyTwoUpsideDownT"
 )
 
 // GetMovedCoords assumes the center of even numbers is moved to the left and
